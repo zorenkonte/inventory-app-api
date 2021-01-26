24ecdf33e00443405da5ba46417f7e6261279151
DarkMode : On <muffintoppings36@gmail.com>
2021-01-26 17:51:43 +0800

    Initial dependency and source file

---
+++ src/test/java/com/darkmode/inventory_app/InventoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkmode.inventory_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
